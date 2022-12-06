5400d312394557886d5a9e32f07adae5df7ebdc7
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2022-12-06 21:48:55 +0100

    add javascript

---
+++ main.js
@@ -0,0 +1,6 @@
+// DROPDOWN FILTER INTERACTION
+
+const selectBtn = document.querySelector (".select-btn"),
+items = document.querySelector (".item");
+
+console.log(selectBtn,item)
--- menu.js
+++ menu.js
@@ -12,9 +12,3 @@ const toggleMenu = () => {
     }
 }
 
-// DROPDOWN FILTER INTERACTION
-
-const selectBtn = document.querySelector (".select-btn"),
-items = document.querySelectorAll (".item");
-
-console.log(selectBtn,);
