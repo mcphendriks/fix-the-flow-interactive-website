8d9f90114b48ee2d0da0e85c322bbb7e10c66b9e
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2022-12-19 13:19:29 +0100

    Add checkbox display

--- scripts/script.js
+++ scripts/script.js
@@ -12,24 +12,48 @@ selectBtn.addEventListener("click", () => {
   selectBtn.classList.toggle("open");
 });
 
-// Responsible for selecting an classnames(elemen)t from a document by using a CSS selector
-const easyCheckbox = document.querySelector('.easy-care-checkbox');
-const hardCheckbox = document.querySelector('.hard-care-checkbox');
+// Responsible for the multi-select checkboxes, to select plants on care difficulty
 
-// console.log(easyCheckbox)
-// console.log(hardCheckbox)
+// Responsible to get the checkbox 
+const easyFilter = document.getElementById('easy-filter')
+const hardFilter = document.getElementById('hard-filter')
+
+console.log(easyFilter,hardFilter)
+
+const plants = document.querySelectorAll('.card-plant')
+
+console.log(plants)
+
+easyFilter.addEventListener('change', () => {
+  plants.forEach(plant => {
+    if (plant.classlist.contains('easy')) 
+    {
+      plant.style.display = 
+      easyFilter.checked ? 'block' : 'none';
+    }
+  })
+})
+
+hardFilter.addEventListener('change', () => {
+  plants.forEach(plant => {
+    if (plant.classlist.contains('hard'))
+    {
+      plant.style.display =
+      hardFilter.checked ? 'block' : 'none'
+    }
+  } )
+})
 
 
-// Responsible for the card elements 
-
-const cardPlant = document.querySelectorAll('.card-plant');
-
-// console.log(cardPlant)
-
-// Listen for changes in the checkbox input elements
 
 
 
+
+
+
+
+  
+ 
 
 
 
