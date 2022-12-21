2bceeae7e06f4cdbf2f5260a144fe95b2727e6c0
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2022-12-21 18:38:05 +0100

    add js get the checkbox

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@
 let selectBtn = document.querySelector('.select-btn');
 items = document.querySelectorAll(".item");
 
-// console.log(select-btn,items) delete after checking
+console.log(select-btn,items) 
 
 // Responsible to attach an click (event handler) to an dropdown icon( element.) 
 selectBtn.addEventListener("click", () => {
@@ -15,35 +15,9 @@ selectBtn.addEventListener("click", () => {
 // Responsible for the multi-select checkboxes, to select plants on care difficulty
 
 // Responsible to get the checkbox 
-const easyFilter = document.getElementById('easy-filter')
-const hardFilter = document.getElementById('hard-filter')
-
-console.log(easyFilter,hardFilter)
-
-const plants = document.querySelectorAll('.card-plant')
-
-console.log(plants)
-
-easyFilter.addEventListener('change', () => {
-  plants.forEach(plant => {
-    if (plant.classlist.contains('easy')) 
-    {
-      plant.style.display = 
-      easyFilter.checked ? 'block' : 'none';
-    }
-  })
-})
-
-hardFilter.addEventListener('change', () => {
-  plants.forEach(plant => {
-    if (plant.classlist.contains('hard'))
-    {
-      plant.style.display =
-      hardFilter.checked ? 'block' : 'none'
-    }
-  } )
-})
-
+var plantsCards = document.querySelectorAll('.card-plant')
+let easyCheckbox = document.querySelector('input.easy-checkbox')
+let hardCheckbox = document.querySelector('input.hard-checkbox')
 
 
 
