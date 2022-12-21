eeeae98ace13e2f65fa8cd5d0a8fbb843ce519d5
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2022-12-21 19:21:56 +0100

    add js select checkbox

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,10 @@
 
-
 // Responsible for selecting an element from a document by using a CSS selector
 
 let selectBtn = document.querySelector('.select-btn');
 items = document.querySelectorAll(".item");
 
-console.log(select-btn,items) 
+// console.log(select-btn,items) delete after checking
 
 // Responsible to attach an click (event handler) to an dropdown icon( element.) 
 selectBtn.addEventListener("click", () => {
@@ -14,15 +13,28 @@ selectBtn.addEventListener("click", () => {
 
 // Responsible for the multi-select checkboxes, to select plants on care difficulty
 
-// Responsible to get the checkbox 
-var plantsCards = document.querySelectorAll('.card-plant')
+// Responsible to select all elements with the class "card-plant"
+var plantsCards = document.querySelectorAll(".card-plant") 
 let easyCheckbox = document.querySelector('input.easy-checkbox')
 let hardCheckbox = document.querySelector('input.hard-checkbox')
 
+//  console for debugging purposes
+console.log(plantsCards,easyCheckbox,hardCheckbox)
 
-
-
-
+// Responsible for adding an event listener to the easyCheckbox element that listens for a "click" event. 
+easyCheckbox.addEventListener("click", () => {
+ plantsCards.forEach(plant => {  
+    if(plant.classList.contains("easy")) {  // Responsible for looping through each element in the plantsCards node list. 
+        plant.classList.toggle("active") // Responsible for checking if it has the class "easy". If it does, it toggles the "active" class on that element.
+         }}) 
+        })
+         
+hardCheckbox.addEventListener("click", () => { 
+   plantsCards.forEach(plant => {   
+    if(plant.classList.contains("hard")) {      
+        plant.classList.toggle("active")   
+      }}) 
+    }) 
 
 
 
